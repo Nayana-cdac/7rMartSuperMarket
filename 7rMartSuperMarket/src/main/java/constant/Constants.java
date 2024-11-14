8f5f661bfa07b3a4916536096d87874d1e29bcb0
Aneesh Kumar K B <aneesh_kb@cdac.in>
2024-11-14 10:43:43 +0530

    Added to 7rMartSuperMarket repo

---
+++ 7rMartSuperMarket/src/main/java/constant/Constants.java
@@ -0,0 +1,9 @@
+package constant;
+
+public class Constants {
+	public static final String CONFIGFILE = System.getProperty("user.dir") 
+ 			+ "\\src\\main\\resources\\Config.properties";
+	public static final String ERRORMESSAGEFORLOGINPAGE = "Error message for Login page";
+	public static final String TESTDATAFILE =  System.getProperty("user.dir") 
+ 			+ "\\src\\main\\resources\\userdata.xlsx";
+}
